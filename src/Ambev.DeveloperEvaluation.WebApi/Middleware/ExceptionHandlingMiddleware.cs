38adc266d7c9970f4edd48010f3f233ecb4ccfe1
Breno Baldovinotti <breno.baldovinotti@windifferent.com>
2025-01-21 01:10:44 -0300

    Improve validation middleware to deal with exceptions as well

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using Ambev.DeveloperEvaluation.Common.Models;
+using Ambev.DeveloperEvaluation.Common.Validation;
+using FluentValidation;
+using System.Text.Json;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException ex)
+        {
+            await HandleValidationExceptionAsync(context, ex);
+        }
+        catch (InvalidOperationException ex)
+        {
+            await HandleInvalidOperationExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleValidationExceptionAsync(HttpContext context, ValidationException exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        var response = new ApiResponseModel
+        {
+            Success = false,
+            Message = "Validation Failed",
+            Errors = exception.Errors
+                .Select(error => (ValidationErrorDetail)error)
+        };
+
+        var jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response, jsonOptions));
+    }
+
+    private static Task HandleInvalidOperationExceptionAsync(HttpContext context, InvalidOperationException exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        var response = new ApiResponseModel
+        {
+            Success = false,
+            Message = exception.Message // Use the exception's message as the response message
+        };
+
+        var jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response, jsonOptions));
+    }
+}
